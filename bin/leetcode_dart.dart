0a10f3482bb74e30487441ccaa3be0966eb69084
regso <55665811+regso@users.noreply.github.com>
2024-04-24 09:04:13 +0300

    Init.

---
+++ bin/leetcode_dart.dart
@@ -0,0 +1,5 @@
+import 'package:leetcode_dart/leetcode_dart.dart' as leetcode_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${leetcode_dart.calculate()}!');
+}
---
+++ test/leetcode_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:leetcode_dart/leetcode_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
